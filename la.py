e8f9faf4d5d3c224fa455a32d503d02584395e82
Kittisak Maneewong <kittisak.ma.59@ubu.ac.th>
2019-07-18 12:14:02 +0700

    guass18072019

--- la.py
+++ la.py
@@ -1,9 +1,38 @@
 from mat import *
-
+import numpy as np
 A = readm('A.csv')
 b = readm('b.csv')
 
 def solve(A, b):
+    """
+    using Gauss method with numpy
+    1 กำจัดจุดอ่อน eliminate
+    2 แทนค่าย้อนกลับ back substitution
+    """
+    a,b = np.array(A),np.array(b)
+
+    #1. eliminate
+    n = len(a)
+    #print(f'n={n}')
+    for k in range(0, n-1): #pivot eq
+       #print(f'k={k}')
+        for j in range(k+1, n):
+            
+            if a[j,k] != 0.0:
+                lam = a[j,k]/a[k,k]
+                #update A[j][k เป็นต้นไป]
+                a[j,k:n] = a[j, k:n] - lam*a[k,k:n] #ไล่ลูปแบบเวกเตอร์
+                b[j] = b[j] - lam*b[k] #update b[j]
+
+    #2. back substitution
+    x = np.array([0]*n)
+    for k in range(n-1, -1, -1):
+        x[k] = (b[k] - np.dot(a[k,k+1:n], x[k+1:n]))/a[k,k]
+
     # YOUR CODE HERE
-    # x = []
-    return x
+    
+    return x.flatten()
+printm(A)
+printm(b)
+print(solve(A,b))
+
